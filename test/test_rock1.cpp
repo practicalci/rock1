94cc86698bdec2e53ea1d06e690afbc9d5d8027b
Mario Costa <you@example.com>
2019-01-03 10:40:10 +0000

    Initial commit.

--- test/test_rock1.cpp
+++ test/test_rock1.cpp
@@ -1,6 +1,4 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp" 
+#include "catch.hpp"
 
-TEST_CASE("Rock1Test", "[all]") {
-    REQUIRE( 1 == 1 );
-}
+TEST_CASE("Rock1Test", "[all]") { REQUIRE(1 == 1); }
